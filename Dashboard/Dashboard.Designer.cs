cd99c4467848b05343a058df2b1775dcad786fb5
chau minh duc <19521371@gm.uit.edu.vn>
2021-05-24 21:00:07 +0700

    Add dashboard form

---
+++ Dashboard/Dashboard.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Dashboard
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_bai1 = new System.Windows.Forms.Button();
+            this.btn_bai2 = new System.Windows.Forms.Button();
+            this.btn_bai3 = new System.Windows.Forms.Button();
+            this.btn_bai4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhóm 5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(185, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Châu Minh Đức - 19521371";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(177, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Đoàn Tưởng Linh - 17520682";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(170, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Phạm Hoàng Dũng - 17520379";
+            // 
+            // btn_bai1
+            // 
+            this.btn_bai1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bai1.Location = new System.Drawing.Point(61, 239);
+            this.btn_bai1.Name = "btn_bai1";
+            this.btn_bai1.Size = new System.Drawing.Size(225, 51);
+            this.btn_bai1.TabIndex = 1;
+            this.btn_bai1.Text = "Bài 1";
+            this.btn_bai1.UseVisualStyleBackColor = true;
+            this.btn_bai1.Click += new System.EventHandler(this.btn_bai1_Click);
+            // 
+            // btn_bai2
+            // 
+            this.btn_bai2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bai2.Location = new System.Drawing.Point(349, 239);
+            this.btn_bai2.Name = "btn_bai2";
+            this.btn_bai2.Size = new System.Drawing.Size(225, 51);
+            this.btn_bai2.TabIndex = 1;
+            this.btn_bai2.Text = "Bài 2";
+            this.btn_bai2.UseVisualStyleBackColor = true;
+            this.btn_bai2.Click += new System.EventHandler(this.btn_bai2_Click);
+            // 
+            // btn_bai3
+            // 
+            this.btn_bai3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bai3.Location = new System.Drawing.Point(61, 329);
+            this.btn_bai3.Name = "btn_bai3";
+            this.btn_bai3.Size = new System.Drawing.Size(225, 51);
+            this.btn_bai3.TabIndex = 1;
+            this.btn_bai3.Text = "Bài 3";
+            this.btn_bai3.UseVisualStyleBackColor = true;
+            this.btn_bai3.Click += new System.EventHandler(this.btn_bai3_Click);
+            // 
+            // btn_bai4
+            // 
+            this.btn_bai4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bai4.Location = new System.Drawing.Point(349, 329);
+            this.btn_bai4.Name = "btn_bai4";
+            this.btn_bai4.Size = new System.Drawing.Size(225, 51);
+            this.btn_bai4.TabIndex = 1;
+            this.btn_bai4.Text = "Bài 4";
+            this.btn_bai4.UseVisualStyleBackColor = true;
+            this.btn_bai4.Click += new System.EventHandler(this.btn_bai4_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 435);
+            this.Controls.Add(this.btn_bai4);
+            this.Controls.Add(this.btn_bai3);
+            this.Controls.Add(this.btn_bai2);
+            this.Controls.Add(this.btn_bai1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard - Web Server";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_bai1;
+        private System.Windows.Forms.Button btn_bai2;
+        private System.Windows.Forms.Button btn_bai3;
+        private System.Windows.Forms.Button btn_bai4;
+    }
+}
+
--- Dashboard/Form_Bai1.cs
+++ Dashboard/Form_Bai1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Dashboard
 {
-    public partial class Form1 : Form
+    public partial class Form_Bai1 : Form
     {
-        public Form1()
+        public Form_Bai1()
         {
             InitializeComponent();
         }
