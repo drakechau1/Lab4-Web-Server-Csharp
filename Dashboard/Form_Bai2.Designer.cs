5b8130f6c31dee63b10ad7d68b35e10a16112378
chau minh duc <19521371@gm.uit.edu.vn>
2021-05-25 13:45:23 +0700

    Done ex 2

--- Dashboard/Form_Bai2.Designer.cs
+++ Dashboard/Form_Bai2.Designer.cs
@@ -29,12 +29,104 @@ namespace Dashboard
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_URL = new System.Windows.Forms.TextBox();
+            this.txt_command = new System.Windows.Forms.TextBox();
+            this.rich_html = new System.Windows.Forms.RichTextBox();
+            this.btn_post = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Command";
+            // 
+            // txt_URL
+            // 
+            this.txt_URL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_URL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_URL.Location = new System.Drawing.Point(137, 36);
+            this.txt_URL.Name = "txt_URL";
+            this.txt_URL.Size = new System.Drawing.Size(498, 30);
+            this.txt_URL.TabIndex = 1;
+            // 
+            // txt_command
+            // 
+            this.txt_command.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_command.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_command.Location = new System.Drawing.Point(137, 75);
+            this.txt_command.Name = "txt_command";
+            this.txt_command.Size = new System.Drawing.Size(498, 30);
+            this.txt_command.TabIndex = 1;
+            this.txt_command.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_command_KeyPress);
+            // 
+            // rich_html
+            // 
+            this.rich_html.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rich_html.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rich_html.Location = new System.Drawing.Point(31, 132);
+            this.rich_html.Name = "rich_html";
+            this.rich_html.Size = new System.Drawing.Size(731, 415);
+            this.rich_html.TabIndex = 2;
+            this.rich_html.Text = "";
+            // 
+            // btn_post
+            // 
+            this.btn_post.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_post.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_post.Location = new System.Drawing.Point(666, 28);
+            this.btn_post.Name = "btn_post";
+            this.btn_post.Size = new System.Drawing.Size(96, 41);
+            this.btn_post.TabIndex = 3;
+            this.btn_post.Text = "POST";
+            this.btn_post.UseVisualStyleBackColor = true;
+            this.btn_post.Click += new System.EventHandler(this.btn_post_Click);
+            // 
+            // Form_Bai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(790, 559);
+            this.Controls.Add(this.btn_post);
+            this.Controls.Add(this.rich_html);
+            this.Controls.Add(this.txt_command);
+            this.Controls.Add(this.txt_URL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_Bai2";
             this.Text = "Form_Bai2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_URL;
+        private System.Windows.Forms.TextBox txt_command;
+        private System.Windows.Forms.RichTextBox rich_html;
+        private System.Windows.Forms.Button btn_post;
     }
 }
--- Dashboard/Form_Bai2.cs
+++ Dashboard/Form_Bai2.cs
@@ -1,20 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace Dashboard
 {
     public partial class Form_Bai2 : Form
     {
+        /// <summary>
+        /// Child functions
+        /// </summary>
+
+        public string Post_HTML(string url, string command)
+        {
+            byte[] arr_bytes = Encoding.UTF8.GetBytes(command);
+            try
+            {
+                // Web request
+                WebRequest web_request = WebRequest.Create(url);
+                web_request.Credentials = CredentialCache.DefaultCredentials;
+                web_request.Method = "POST";
+                web_request.ContentLength = arr_bytes.Length;
+                web_request.ContentType = "application/x-www-form-urlencoded";
+
+                Stream stream = web_request.GetRequestStream();
+                stream.Write(arr_bytes, 0, arr_bytes.Length);
+                stream.Close();
+
+                // Web response
+                WebResponse web_response = web_request.GetResponse();
+                Console.WriteLine(((HttpWebResponse)web_response).StatusDescription);
+                stream = web_response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string html = reader.ReadToEnd();
+
+                stream.Close();
+                web_response.Close();
+
+                return html;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Main functions
+        /// </summary>
+
         public Form_Bai2()
         {
             InitializeComponent();
+        }
+
+        private void btn_post_Click(object sender, EventArgs e)
+        {
+            if (txt_URL.Text != string.Empty)
+            {
+                rich_html.Text = Post_HTML(txt_URL.Text, txt_command.Text);
+            }
+        }
+
+        private void txt_command_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (txt_URL.Text != string.Empty)
+                {
+                    rich_html.Text = Post_HTML(txt_URL.Text, txt_command.Text);
+                }
+            }
         }
     }
 }
