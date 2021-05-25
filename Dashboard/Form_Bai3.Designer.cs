6afa78fbddbaf13e79ab29f0bac0ad36ed856382
chau minh duc <19521371@gm.uit.edu.vn>
2021-05-25 15:15:47 +0700

    Done ex 3

--- Dashboard/Form_Bai3.Designer.cs
+++ Dashboard/Form_Bai3.Designer.cs
@@ -29,12 +29,104 @@ namespace Dashboard
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_URL = new System.Windows.Forms.TextBox();
+            this.txt_savedPath = new System.Windows.Forms.TextBox();
+            this.btn_download = new System.Windows.Forms.Button();
+            this.rich_html = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Saved path";
+            // 
+            // txt_URL
+            // 
+            this.txt_URL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_URL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_URL.Location = new System.Drawing.Point(137, 21);
+            this.txt_URL.Name = "txt_URL";
+            this.txt_URL.Size = new System.Drawing.Size(482, 30);
+            this.txt_URL.TabIndex = 1;
+            // 
+            // txt_savedPath
+            // 
+            this.txt_savedPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_savedPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_savedPath.Location = new System.Drawing.Point(137, 67);
+            this.txt_savedPath.Name = "txt_savedPath";
+            this.txt_savedPath.Size = new System.Drawing.Size(482, 30);
+            this.txt_savedPath.TabIndex = 1;
+            this.txt_savedPath.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txt_savedPath_MouseClick);
+            // 
+            // btn_download
+            // 
+            this.btn_download.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_download.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_download.Location = new System.Drawing.Point(637, 17);
+            this.btn_download.Name = "btn_download";
+            this.btn_download.Size = new System.Drawing.Size(130, 38);
+            this.btn_download.TabIndex = 2;
+            this.btn_download.Text = "Download";
+            this.btn_download.UseVisualStyleBackColor = true;
+            this.btn_download.Click += new System.EventHandler(this.btn_download_Click);
+            // 
+            // rich_html
+            // 
+            this.rich_html.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rich_html.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rich_html.Location = new System.Drawing.Point(21, 124);
+            this.rich_html.Name = "rich_html";
+            this.rich_html.Size = new System.Drawing.Size(746, 423);
+            this.rich_html.TabIndex = 3;
+            this.rich_html.Text = "";
+            // 
+            // Form_Bai3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(786, 559);
+            this.Controls.Add(this.rich_html);
+            this.Controls.Add(this.btn_download);
+            this.Controls.Add(this.txt_savedPath);
+            this.Controls.Add(this.txt_URL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_Bai3";
             this.Text = "Form_Bai3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_URL;
+        private System.Windows.Forms.TextBox txt_savedPath;
+        private System.Windows.Forms.Button btn_download;
+        private System.Windows.Forms.RichTextBox rich_html;
     }
 }
--- Dashboard/Form_Bai3.cs
+++ Dashboard/Form_Bai3.cs
@@ -1,20 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace Dashboard
 {
     public partial class Form_Bai3 : Form
     {
+        /// <summary>
+        /// Child functions
+        /// </summary>
+        
+        public string Get_HTML(string url)
+        {
+            string html = string.Empty;
+
+            try
+            {
+                WebClient web_client = new WebClient();
+                web_client.Credentials = CredentialCache.DefaultCredentials;
+
+                Stream stream = web_client.OpenRead(url);
+                StreamReader reader = new StreamReader(stream);
+
+                html = reader.ReadToEnd();
+
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+            return html;
+        }
+
+        public void Write_File(string path, string data)
+        {
+            if (File.Exists(txt_savedPath.Text))
+            {
+                File.Delete(txt_savedPath.Text);
+            }
+
+            StreamWriter writer = File.CreateText(txt_savedPath.Text);
+            writer.WriteLine(data);
+            writer.Flush();
+            writer.Close();
+
+            MessageBox.Show("Downloaded", "Success");
+        }
+
+        /// <summary>
+        /// Main functions
+        /// </summary>
+
         public Form_Bai3()
         {
             InitializeComponent();
+        }
+
+        private void btn_download_Click(object sender, EventArgs e)
+        {
+            string file_path = txt_savedPath.Text;
+            if (txt_URL.Text != string.Empty)
+            {
+                if (file_path == string.Empty)
+                {
+                    MessageBox.Show("Saved path is empty", "Error");
+                }
+                else
+                { 
+                    string html = Get_HTML(txt_URL.Text);
+
+                    rich_html.Text = html;
+
+                    Write_File(file_path, html);
+                }
+            }
+        }
+
+        private void txt_savedPath_MouseClick(object sender, MouseEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "Hypertext Transfer Protocol | *.html";
+            sfd.DefaultExt = "html";
+
+            sfd.ShowDialog();
+            if (sfd.FileName != string.Empty)
+            {
+                txt_savedPath.Text = sfd.FileName;
+            }
         }
     }
 }
