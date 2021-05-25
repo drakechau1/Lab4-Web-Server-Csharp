f2951061dc4a80c1ad5d6a1a2b320c9186f3b5c2
chau minh duc <19521371@gm.uit.edu.vn>
2021-05-25 12:42:27 +0700

    Done ex 1

--- Dashboard/Form_Bai1.Designer.cs
+++ Dashboard/Form_Bai1.Designer.cs
@@ -29,12 +29,65 @@ namespace Dashboard
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_URL = new System.Windows.Forms.TextBox();
+            this.btn_get = new System.Windows.Forms.Button();
+            this.rich_html = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txt_URL
+            // 
+            this.txt_URL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_URL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_URL.Location = new System.Drawing.Point(26, 20);
+            this.txt_URL.Name = "txt_URL";
+            this.txt_URL.Size = new System.Drawing.Size(623, 30);
+            this.txt_URL.TabIndex = 0;
+            // 
+            // btn_get
+            // 
+            this.btn_get.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_get.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_get.Location = new System.Drawing.Point(669, 14);
+            this.btn_get.Name = "btn_get";
+            this.btn_get.Size = new System.Drawing.Size(91, 43);
+            this.btn_get.TabIndex = 1;
+            this.btn_get.Text = "GET";
+            this.btn_get.UseVisualStyleBackColor = true;
+            this.btn_get.Click += new System.EventHandler(this.btn_get_Click);
+            // 
+            // rich_html
+            // 
+            this.rich_html.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rich_html.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rich_html.Location = new System.Drawing.Point(26, 75);
+            this.rich_html.Name = "rich_html";
+            this.rich_html.ReadOnly = true;
+            this.rich_html.Size = new System.Drawing.Size(734, 457);
+            this.rich_html.TabIndex = 2;
+            this.rich_html.Text = "";
+            // 
+            // Form_Bai1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 552);
+            this.Controls.Add(this.rich_html);
+            this.Controls.Add(this.btn_get);
+            this.Controls.Add(this.txt_URL);
+            this.Name = "Form_Bai1";
             this.Text = "Form_Bai1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_URL;
+        private System.Windows.Forms.Button btn_get;
+        private System.Windows.Forms.RichTextBox rich_html;
     }
 }
--- Dashboard/Form_Bai1.cs
+++ Dashboard/Form_Bai1.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace Dashboard
 {
     public partial class Form_Bai1 : Form
     {
+
+
+        /// <summary>
+        /// Child functions
+        /// </summary>
+
+        public string Get_HTML(string url)
+        {
+            try
+            {
+                WebRequest web_request = WebRequest.Create(url);
+                web_request.Credentials = CredentialCache.DefaultCredentials;
+                WebResponse web_response = web_request.GetResponse();
+                Stream stream = web_response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(stream);
+
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Main functions
+        /// </summary>
+
         public Form_Bai1()
         {
             InitializeComponent();
+        }
+
+        private void btn_get_Click(object sender, EventArgs e)
+        {
+            if (txt_URL.Text != string.Empty)
+            {
+                rich_html.Text = Get_HTML(txt_URL.Text);
+            }
         }
     }
 }
